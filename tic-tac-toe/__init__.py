1448958cc6d585d7fb0f2f62dd90aabc8f0f368d
sujan poudel <spoudel347@gmail.com>
2018-07-24 18:57:20 +0545

    file header inserted

---
+++ tic-tac-toe/__init__.py
@@ -0,0 +1,9 @@
+'''
+File: __init__.py
+Project: min-max-implementations/tic-tac-toe
+File Created: Tuesday, 10th July 2018 11:36:15 am
+Author: Sujan Poudel (https://github.com/psuzn)
+-----
+Last Modified: Tuesday, 24th July 2018 6:55:06 pm
+Modified By: Sujan Poudel 
+'''
---
+++ tic-tac-toe/main.py
@@ -0,0 +1,9 @@
+'''
+File: main.py
+Project: min-max-implementations/tic-tac-toe
+File Created: Tuesday, 24th July 2018 6:50:44 pm
+Author: Sujan Poudel (https://github.com/psuzn)
+-----
+Last Modified: Tuesday, 24th July 2018 6:55:10 pm
+Modified By: Sujan Poudel 
+'''
